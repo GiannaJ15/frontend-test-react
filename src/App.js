514f1f0aeb5249cef6cce43401077cbeab14de63
aine <aineekohh@yahoo.com>
2020-06-30 14:10:07 -0500

    added state,props,update. Still need to add delete and etc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { TodoList } from './components/todos';
 import { todos } from './data/todos';
 
-function App() {
+class App extends Component {
+  // Creating a constructor that will have props and super
+  constructor(props){
+    super(props);
 
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+
+  // Create new Item to todo list
+
+  updateInput(key, value){
+    this.setState({
+      [key]: value
+    });
+  }
+  addItem(){
+    const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //
+    const list = [...this.state.list];
+    //add item to todo list
+    list.push(newItem);
+    //updat and reset
+    this.setState({
+      list,
+      newItem:""
+    });
+
+
+
+
+  }
+
+render() {
   return (
     <div className="App">
       <TodoList  todos={todos} />
     </div>
   );
 }
-
+}
 export default App;
--- src/components/todos/TodoList.js
+++ src/components/todos/TodoList.js
@@ -19,13 +19,18 @@ export function TodoList() {
                     <div
                         className="flex my-8"
                     >
+
                         <input
                             className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-gray-darker"
                             placeholder="Add Todo"
+                            value={this.state.newItem}
+                            onChange={e => this.updateInput("newItem", e.target.value)}
+
                         />
                         <button
                             className="flex-no-shrink p-2 border-2 rounded text-teal-700 border-teal-700 hover:text-white hover:bg-teal-700"
-                        >
+                             onClick={() => this.addItem()}
+                      >
                             Add
                         </button>
                     </div>
